adcd5b182b28273b0226cc6de1eb0c266c0bcab9
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-09-07 23:20:34 -0600

    modified:   Section20/Iterators/src/main.cpp 	modified:   Section20/Iterators/src/main.exe

--- Section20/Iterators/src/main.cpp
+++ Section20/Iterators/src/main.cpp
@@ -30,9 +30,39 @@ void test1() {
   std::cout << "=======================" << std::endl;
 }
 
+void test2() {
+  std::cout << "\n=======================" << std::endl;
+
+  std::vector<int> nums1 {1, 2, 3, 4, 5};
+  std::vector<int>::iterator it = nums1.begin();
+
+  while (it != nums1.end()) {
+    std::cout << *it << std::endl;
+    it++;
+  }
+
+  std::cout << "=======================" << std::endl;
+}
+
+void test3() {
+  std::cout << "\n=======================" << std::endl;
+
+  std::vector<int> nums1 {1, 2, 3, 4, 5};
+  std::vector<int>::const_iterator it1 = nums1.begin();
+
+  while (it1 != nums1.end()) {
+    std::cout << *it1 << std::endl;
+    it1++;
+  }
+
+  std::cout << "=======================" << std::endl;
+}
+
+
 int main() {
 
-  test1();
+  // test1();
+  test2();
 
   return 0;
 }
